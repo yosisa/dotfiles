72aac3f23a91316070fe069cd2abc9aeaa14527f
Yoshihisa Tanaka <yt.hisa@gmail.com>
2022-01-06 21:01:18 +0900

    zsh: Update aliases

--- .zsh.d/alias.zsh
+++ .zsh.d/alias.zsh
@@ -1,5 +1,22 @@
-if command -v gls > /dev/null; then
+if command -v exa >/dev/null; then
+    alias ls="exa"
+    alias ll="exa -l"
+elif command -v gls >/dev/null; then
     alias ls="gls --color"
+    alias ll="gls --color -l"
 else
     alias ls="ls -G"
+    alias ll="ls -Gl"
 fi
+
+if command -v nvim >/dev/null; then
+    alias v="nvim"
+elif command -v vim >/dev/null; then
+    alias v="vim"
+fi
+
+command -v kubectl >/dev/null && alias -g k="kubectl"
+command -v kubectx >/dev/null && alias kx="kubectx"
+
+command -v docker >/dev/null && alias d="docker"
+command -v nerdctl >/dev/null && alias n="nerdctl"
