9c390a4082eb40332030bf8e53ffd17b84ddf6ec
Yoshihisa Tanaka <yt.hisa@gmail.com>
2026-07-02 23:54:01 +0900

    zsh: update aliases

--- .zsh.d/alias.zsh
+++ .zsh.d/alias.zsh
@@ -1,12 +1,15 @@
-if command -v exa >/dev/null; then
-    alias ls="exa"
-    alias ll="exa -l"
+if command -v eza >/dev/null; then
+    alias ls="eza"
+    alias ll="eza -l"
+    alias lal="eza -al"
 elif command -v gls >/dev/null; then
     alias ls="gls --color"
     alias ll="gls --color -l"
+    alias lal="gls --color -Al"
 else
     alias ls="ls -G"
     alias ll="ls -Gl"
+    alias lal="ls -GAl"
 fi
 
 if command -v nvim >/dev/null; then
@@ -19,10 +22,21 @@ elif command -v vim >/dev/null; then
     alias vy="vim -c 'set ft=yaml'"
 fi
 
+command -v gsed >/dev/null && alias sed="gsed"
+
 command -v kubectl >/dev/null && alias k="kubectl"
 command -v kubectx >/dev/null && alias kx="kubectx"
 
 command -v docker >/dev/null && alias d="docker"
 command -v nerdctl >/dev/null && alias n="nerdctl"
 
+command -v gitui >/dev/null && alias g="gitui"
 command -v just >/dev/null && alias j="just"
+
+command -v claude >/dev/null && alias cl="claude"
+command -v codex >/dev/null && alias cx="codex"
+command -v opencode >/dev/null && alias oc="opencode"
+
+if command -v pi >/dev/null; then
+    command -v nono >/dev/null && alias px="nono run --profile always-further/pi --allow-cwd -- pi"
+fi
